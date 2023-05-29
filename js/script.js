a40d782e89e4a4fd330a927c2d1568f37e4d6c5a
Mario-Marchio <mariomarchio1610@gmail.com>
2023-05-29 16:18:56 +0200

    milestone 1 done

--- js/script.js
+++ js/script.js
@@ -1,45 +1,51 @@
 console.log(`JS OK`);
 
 // recupero placeholder dal dom 
-const placeholder = document.getElementById(`costoFinale`);
-console.log(costoFinale);
-
-// chiedi al utente l'eta
-const age = prompt(`inserisci la tua età`,`15`);
+const button = document.getElementById(`button`);
+console.log(button);
+const nameInput = document.getElementById(`nome`);
+console.log(name);
+const ageInput = document.getElementById(`age`);
 console.log(age);
-
-// chiedi al utente il numero di km che vuole percorrere
-const km = prompt(`inserisci il numero di km che vuoi percorrere`,`100`);
+const kmInput = document.getElementById(`km`);
 console.log(km);
 
-// calcolo prezzo del biglietto senza sconti
-const price = 0.21 * km;
-console.log(price);
+button.addEventListener(`click`, function () {
 
-// calcolare gli sconti 
-let discount
-if(age<=18){
-    discount = price * 20 /100;
-    console.log(discount);
-}else if (age>65){
-    discount = price * 40 /100;
-    console.log(discount);
-}
+    // calcolo prezzo del biglietto senza sconti 
 
-// applicare lo sconto
-let realPrice
-if(age<=18){
-    realPrice = price - discount;
-    realPrice = realPrice.toFixed(2)
-    console.log(realPrice);
-}else if (age>65){
-    realPrice = price - discount;
-    realPrice = realPrice.toFixed(2)
-    console.log(realPrice);
-}else{
-    realPrice = price.toFixed(2);
-    console.log(realPrice);
-}
+    const price = 0.21 * kmInput.value;
+    console.log(price);
+
+    // calcolare gli sconti
+    // applicare lo sconto
+
+    let discount
+    let realPrice
+    if(ageInput.value<=18){
+        discount = price * 20 /100;
+        console.log(discount);
+
+        realPrice = price - discount;
+        realPrice = realPrice.toFixed(2)
+        console.log(realPrice);
+
+    }else if (ageInput.value>65){
+        discount = price * 40 /100;
+        console.log(discount);
+
+        realPrice = price - discount;
+        realPrice = realPrice.toFixed(2)
+        console.log(realPrice);
+
+    }else{
+        realPrice = price.toFixed(2);
+        console.log(realPrice);
+    }
+
+
+});
+
 
 // mostrala a schermo
-placeholder.innerText = realPrice;
+// placeholder.innerText = realPrice;
